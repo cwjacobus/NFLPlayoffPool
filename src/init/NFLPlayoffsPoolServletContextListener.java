0afca54ec7263490054a8e671287a52650f5d6a5
Chris Jacobus <cjacobus65@gmail.com>
2022-02-05 11:47:38 -0600

    get db info from props

--- src/init/NFLPlayoffsPoolServletContextListener.java
+++ src/init/NFLPlayoffsPoolServletContextListener.java
@@ -1,16 +1,29 @@
 package init;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 import javax.servlet.ServletContext;  
 import javax.servlet.ServletContextEvent;  
-import javax.servlet.ServletContextListener;  
+import javax.servlet.ServletContextListener;
+
 public class NFLPlayoffsPoolServletContextListener implements ServletContextListener{  
       public void contextInitialized(ServletContextEvent event) {  
             ServletContext context = event.getServletContext();  
-            String url = context.getInitParameter("URL");  
-            String database=context.getInitParameter("Database name");  
-            String username=context.getInitParameter("Username");  
-            String password=context.getInitParameter("password");  
-            NFLPlayoffsPoolDatabase mydb  = new NFLPlayoffsPoolDatabase(url+database, username, password);  
+            
+            Properties prop = new Properties();
+            try {
+            	InputStream input = context.getResourceAsStream("/WEB-INF/NFLPlayoffsPool.properties");
+            	prop.load(input);
+            }
+            catch (Exception e) {
+            	e.printStackTrace();
+            }
+            String url = prop.getProperty("db.url"); //context.getInitParameter("URL");  // from web.xml
+            String database = prop.getProperty("db.database");  
+            String username = prop.getProperty("db.username");  
+            String password = prop.getProperty("db.password");  
+            NFLPlayoffsPoolDatabase mydb  = new NFLPlayoffsPoolDatabase(url + database, username, password);  
             context.setAttribute("Database", mydb); 
             System.out.println("context initialized - DB Info: " + url + database + " " + username);
       }  
